94e45af9b70369d049292da695666902a379b232
Tim Zoet <ts.zoet@gmail.com>
2021-06-19 16:01:23 +0200

    Completed the Mixins section

--- snippets/exception_mixin.cpp
+++
@@ -1,9 +0,0 @@
-// Expect any kind of exception.
-expectThrow(callable);
-// Expect a specific exception.
-expectThrow<MyExceptionClass>(callable);
-// Expect one of a number of exceptions.
-expectThrow<E0, E1, E2>(callable);
-
-// Should not throw.
-expectNoThrow(callable);
